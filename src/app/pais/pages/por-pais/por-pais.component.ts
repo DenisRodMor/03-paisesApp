4eca7bd41a9129358015bd217c1ff2b550868100
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-29 16:25:25 -0600

    Llenando la tabla de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -9,8 +10,9 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
 
-  termino : string = '';
-  hayError: boolean = false;
+  termino : string    = '';
+  hayError: boolean   = false;
+  paises  : Country[] = [];
 
   constructor( private paisservice : PaisService) { }
 
@@ -20,13 +22,10 @@ export class PorPaisComponent {
 
     this.paisservice.buscarPais(this.termino).subscribe( (paises) => {
       console.log(paises);
-
-
-
-
-
+      this.paises = paises;
     }, (err) => {
       this.hayError = true;
+      this.paises   = [];
     });
   }
 }
