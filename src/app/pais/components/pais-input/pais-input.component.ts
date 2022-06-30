548c15818842ff0b55a74b749d2126d358527c92
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-30 12:53:28 -0600

    DebounceTime - en el input

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-pais-input',
@@ -6,15 +8,28 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styles: [
   ]
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit{
 
 
   @Output( )  onEnter : EventEmitter<string> = new EventEmitter();
+  @Output() OnDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer : Subject<string> = new Subject(); //debouncer=antirrebote
 
   termino : string    = '';
   constructor() { }
 
+  ngOnInit(): void {
+    this.debouncer.pipe(debounceTime(300)).subscribe(valor => {
+      this.OnDebounce.emit(valor);
+    })
+  }
+
   buscar(){
     this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada(event : any){
+   this.debouncer.next(this.termino);
   }
 }
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -28,4 +28,10 @@ export class PorPaisComponent {
       this.paises   = [];
     });
   }
+
+  sugerencias(termino : string){
+    this.hayError = false;
+    //TODO: crear sugerencias
+
+  }
 }
