4b4594f5addc05174735a27888d8d861d700372b
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-07-04 11:45:51 -0600

    Mostrar paises por region

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+import { region } from '../../interfaces/region.interfaces';
 
 @Component({
   selector: 'app-por-region',
@@ -10,8 +13,10 @@ export class PorRegionComponent {
 
   regiones: string[] = ['EU','EFTA','CARICOM','PA','AU','USAN','EEU','AL','ASEAN','CAIS','CEFTA','NAFTA','SAARC'];
   regionActiva: string = '';
+  hayError: boolean   = false;
+  paises: Country[]=[];
 
-  constructor() { }
+  constructor(private paisservice : PaisService) { }
 
   getClaseCSS(region :  string ) : string {
    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
@@ -19,7 +24,16 @@ export class PorRegionComponent {
 
   activarRegion (region : string){
     this.regionActiva = region;
+    this.hayError=false;
 
+    if ( region === this.regionActiva){return ;}
+    this.paises = [];
     // TODO: hacer el llamado al servicio
+    this.paisservice.buscarRegion(region).subscribe( (paises) => {
+      this.paises = paises;
+    }, (err) => {
+      this.hayError = true;
+      this.paises   = [];
+    });
   }
 }
